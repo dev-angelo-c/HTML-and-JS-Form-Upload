6131cfc8b54dfd006d733acfc3a618095702dd06
Jack Crish <jcrish@gmail.com>
2020-07-31 01:30:09 -0400

    clean up a bit

--- server.js
+++ server.js
@@ -10,17 +10,22 @@ app.use(cors());
 app.use("/", express.static('public'));
 app.use(express.json())
 
-app.post("/image", upload.single('image101'), (req, res) => {
-  console.log(" ROUTE HIT", req.body, req.file);
-  console.log(" query Params ", req.params);
-  res.status(200).json({ go: "boy" });
-});
+//used for html form upload
+app.post("/image", 
+  upload.single('image101'), 
+  (req, res) => {
+    res.status(200).json({ go: "boy" });
+  }
+);
 
-app.post("/dynamic-image", upload.single("image201"), (req, res)=>{
-  console.log(" Create Dynamically ");
-
-  res.status(200).json({yes: "sir"});
-})
+//used for javascript upload
+app.post("/dynamic-image", 
+  upload.single("image201"), 
+  (req, res)=>{
+    console.log(" Create Dynamically ");
+    res.status(200).json({yes: "sir"});
+  }
+)
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
