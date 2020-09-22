f76cc273096648f478e425308b1b6cd98a6dad2a
Jack Crish <jcrish@gmail.com>
2020-09-22 02:38:21 -0400

    update gitignore

--- server.js
+++ server.js
@@ -1,36 +1,16 @@
-const Bundler = require("parcel-bundler");
 const express = require("express")
 const app = express();
 const Path = require("path");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
-
-//https://parceljs.org/api.html
-const entryFiles = Path.join(__dirname, './public/index.html'); //absolute path to file here
-const bundlerOptions = require("./parcel-config");
-
-const cors = require("cors");
 const PORT = 3000;
 
-(async function () {
-  // Initializes a bundler using the entrypoint location and options provided
-  const bundler = new Bundler(entryFiles, bundlerOptions);
-  app.use(bundler.middleware());
 
-  //app.use(cors());
-  app.use(express.static(Path.join(__dirname, "./public/dist/")));
-  app.use(express.json());
-  console.log(" HELLO PARCEL ");
-  // Run the bundler, this returns the main bundle
-  // Use the events if you're using watch mode as this promise will only trigger once and not for every rebuild
-  const bundle = await bundler.bundle();
-})();
+app.use(express.static("./public/dist"));
 
-app.use("*", (req, res, next) => {
-  console.log(" HEY THERE ")
-  res.status(200).send(" BLAH ");
+app.get("/yay", (req, res, next) => {
+  res.status(200).send(" I LOVE YOU ");
 })
-
 //used for html form upload
 app.post("/image", 
   upload.single('image101'), 
